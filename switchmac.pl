fea96a197a66728424e9259bc98ab3b47e423317
Antoine Bodein <bodein.antoine@gmail.com>
2018-07-23 16:00:16 -0400

    Add mac mac spoof

--- switchmac.pl
+++ switchmac.pl
@@ -1,1 +1,23 @@
-/Users/antoine/env/perl/switchmac.pl
+#!/usr/bin/perl -w
+
+@hexa = ('0'..'9','a'..'f'); 
+
+$len = 12;
+$mac = "";
+
+while ($len--){
+	if  ($len%2) {
+		$mac .= ":";
+	}
+	else{
+		for (0..1){$mac .= $hexa[rand @hexa];}
+	}
+}
+$mac =~ s/^.//;
+
+print "new mac : $mac\n";
+system ("sudo ifconfig en0 ether $mac");
+system ("sudo ifconfig en0 ether down");
+system ("sudo ifconfig en0 ether up");
+
+exit;
